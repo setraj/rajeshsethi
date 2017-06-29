54c4d2791c9be5b1391b957d509fd820d5514669
Rajesh Sethi <rajeshsethi.56@gmail.com>
2017-06-29 20:35:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a").on('click', function(event) {
+
+   // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+    
+})
+setTimeout(function(){
+        $("#dev").typed({
+            strings: ["DEVELOPMENT"],
+            typeSpeed: 50, // typing speed
+            backDelay: 250, // pause before backspacing
+            loop: true, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 0);
+
